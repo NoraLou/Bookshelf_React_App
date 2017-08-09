e33adf2447414a2b3dd42a14d58497bb2d6efc3b
NoraLou <nora.humpage@gmail.com>
2017-08-09 15:11:38 -0700

    state update on the shelfChange works

--- src/App.js
+++ src/App.js
@@ -4,34 +4,79 @@ import './App.css'
 import BookShelf from './BookShelf'
 
 class App extends React.Component {
-  state = {
-    books: []
+
+  constructor(props) {
+    super(props);
+    this.state = { books: [] }
+    this.updateBooks = this.updateBooks.bind(this)
   }
-  
+
   componentDidMount(){
     BooksAPI.getAll().then((books)=>{
-      console.log("books :", books)
       this.setState({books})
     })
   }
 
-  updateBooks(book, shelf){
-    BooksAPI.update(book, shelf)
-    const { books } = this.state
-    console.log('calling updateBooks with book : ', book)
-    console.log('all books :', books)
+  updateBooks(bookChange, shelfChange) {
+    BooksAPI.update(bookChange, shelfChange)
+      .then(() => {
+        let currState = this.state
+        this.setState( (currState) => ({
+          books: currState.books.map(b => {
+            if(bookChange.id === b.id){
+              b.shelf = shelfChange
+            }
+            return b;
+          })
+        }));
+      });
+  
+  }
 
-    // let bookId = book.id
-    // state.books.forEach( (b, bookIdx) => {
-    //   console.log(b.title)
-    //   console.log("searchIdx :", bookId)
+    // console.log('this.state', this.state)
+    // const oldState = this.state.books
+    // console.log('oldState :', oldState)
+
+    // const newState = oldState.map(b => {
+    //   if(b.id === book.id)
+    //     b.shelf = shelf
+    //     console.log("b :", b)
     // })
+
+
+
+
+
+    // this.setState( state => ({
+    //   books: state.map(b => {
+    //     if(b.id === book.id)
+    //       b.shelf = shelf
+    //   })
+    // }))
+
+
+
+
+    // console.log('calling updateBooks with book : ', book)
+    // console.log('all books :', books)
+
+    // let changeIdx = book.id
+    // let newState = oldState.map((b) => (
+    //   if(b.id === changeIdx){
+    //     b.shelf = shelf
+    //     console.log("b")
+    //   }
+    // ))
+
+    // this.setState( oldState )
+
+
 
     // get the book from your books array
     // change the shelf to the current shelf
     // update your state object
     // setState to the newState object
-  }
+  
 
   render() {
    return (
@@ -43,7 +88,7 @@ class App extends React.Component {
     </div>
    )
   }
-  
+
 }
 
 export default App
