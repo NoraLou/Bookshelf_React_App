58a9c17d26dbab1734643f52f759e8f5073c7303
NoraLou <nora.humpage@gmail.com>
2017-08-08 20:31:54 -0700

    try to fix spaces

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -3,40 +3,39 @@ import React, { Component } from 'react'
 
 class BookShelf extends Component {
 
-	shelves = [
+  shelves = [
     { title : 'Want To Read', status: 'wantToRead' },
     { title : 'Currently Reading', status: 'currentlyReading' },
     { title : 'Read', status: 'read' }
   ]
 
-	state = {
-		query:''
-	}
+  state = {
+    query:''
+  }
 
-	render () {
-
-		const { books, updateBooks } = this.props
-
-		return (
-			<div className="list-books-content">
+  render () {
+    const { books, updateBooks } = this.props
+    return (
+      <div className="list-books-content">
         {this.shelves.map((shelf) => (
-          <div className="bookshelf" key={shelf.status}>
-            <h2 className="bookshelf-title">{shelf.title}</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                <Books 
-                  books={ books.filter(b => b.shelf === shelf.status)}
-                  updateBooks = { updateBooks }
+        <div className="bookshelf" key={shelf.status}>
+          <h2 className="bookshelf-title">{shelf.title}</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              <Books 
+                books={ books.filter(b => b.shelf === shelf.status)}
+                updateBooks={ updateBooks }
                 />
-              </ol>
+            </ol>
           </div>
-        ))}
+        </div>
+          ))}
       </div>
     )
   }
 }
 
-export default BooksShelf
+export default BookShelf
 
 
 
