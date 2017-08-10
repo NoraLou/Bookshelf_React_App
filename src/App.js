b25604e4b4d8f6ff2ebe6ddacbac3b6daf0b1bde
NoraLou <nora.humpage@gmail.com>
2017-08-10 16:29:10 -0700

    issue assigning a varible to something outside of a promise

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
   
   getBookShelf() {
     BooksAPI.getAll().then((books)=>{
-      console.log("response from GET BOOK SHELF : ", books)
+      console.log("get shelf books:", books)
       this.setState({books})
       // console.log("books :", books)
     })
@@ -42,20 +42,90 @@ class App extends React.Component {
 
   searchBooks(query){
     if(query) {
-      console.log('searchQuery :', query)
+      let currShelves;
       BooksAPI.search(query)
-      .then((resp) => {
-        console.log('response from Search books:', resp)
-        if (resp.error) {
+      .then((response) => {
+        if (response.error) {
           this.setState({ books:[]})
         } else {
-          this.setState({ books:resp} )
+          //get current books on shelves.
+          BooksAPI.getAll().then( books => {
+            console.log("searchBooks :", books)
+            currShelves = books
+            console.log('insideAPI currShelves', currShelves)
+          })
+          console.log('outsideAPI currShelves', currShelves)
+          let shelvedSearch = response.map(respItem => {
+            let match = currShelves.find( book => book.id === respItem.id)
+            let shelf = match ? match.shelf : "none"
+            respItem.shelf = shelf
+            return respItem
+          })
+
+          //update our search Selections with the current shelves.
+          this.setState({ books:shelvedSearch } )
         }
       })
     } else {
       this.setState({ books: [] });
     } 
   }
+
+
+  // searchBooks(query){
+  //   if(query) {
+  //     console.log('searchQuery :', query)
+  //     BooksAPI.search(query)
+  //     .then((resp) => {
+  //       console.log('response from Search books:', resp)
+  //       if (resp.error) {
+  //         this.setState({ books:[]})
+  //       } else {
+  //         //get current books on shelves.
+  //         BooksAPI.getAll().then( books => {
+  //           books.forEach( b => {
+  //             console.log("from search API call bookshelf :", b.title)
+  //             //for each item on the bookshelf
+  //             //see if it is in the search results
+  //             let match = resp.find( r => r.id === b.id )
+  //             //will return undefined if it is not there... 
+  //             // if truthy
+  //             if (match) {
+  //               //assign our response item the corresponding shelf
+  //               match.shelf = b.shelf
+  //             }
+  //           })
+
+  //         })
+  //         //update our search Selections with the current shelves.
+  //         this.setState({ books:resp} )
+  //       }
+  //     })
+  //   } else {
+  //     this.setState({ books: [] });
+  //   } 
+  // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   // searchBooks={this.searchBooks}
 
