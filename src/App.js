73e2b01324085a6670743102286a97d7a297147a
NoraLou <nora.humpage@gmail.com>
2017-08-09 17:55:30 -0700

    add in search component

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
     super(props);
     // this.state = { books: [] }
     this.updateBooks = this.updateBooks.bind(this)
+    this.searchBooks = this.searchBooks.bind(this)
   }
 
   state = {books:[]}
