437c827927fe758c15fad0c258d4f2e2dd37e1f9
NoraLou <nora.humpage@gmail.com>
2017-08-16 12:17:43 -0700

    refactored presentation comps to be stateless and functions

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,43 +1,39 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
+const Book = (props) => {
 
-class Book extends Component {
+  const { book, updateBook } = props
 
-  static propTypes = {
-    book:  PropTypes.object.isRequired,
-    updateBook: PropTypes.func.isRequired
-  }
- 
-  render() {
-
-    const { book, updateBook } = this.props
-    
-    return (
-      <li key={book.id}> 
-        <div className="book">
-          <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url(${book.imageLinks ? book.imageLinks.smallThumbnail : ' '})` }}></div>
-              <div className="book-shelf-changer">
-                <select onChange={(event) => updateBook( book, event.target.value) } value={book.shelf}>
-                  <option value="none" disabled> Move to...</option>
-                  <option value="currentlyReading">Currently Reading</option>
-                  <option value="wantToRead">Want to Read</option>
-                  <option value="read">Read</option>
-                  <option value="none">None</option>
-                </select>
-              </div>
-          </div>
-          <div className="book-title">{book.title}</div>
-          { (book.authors) && (
-            book.authors.map((author)=> (
-              <div className="book-authors" key={author}>{author}</div>
-            ))
-          )}
-        </div> 
-      </li>
-    )
-  }
+  return (
+    <li key={book.id}> 
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url(${book.imageLinks ? book.imageLinks.smallThumbnail : ' '})` }}></div>
+            <div className="book-shelf-changer">
+              <select onChange={(event) => updateBook( book, event.target.value) } value={book.shelf}>
+                <option value="none" disabled> Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+        </div>
+        <div className="book-title">{book.title}</div>
+        { (book.authors) && (
+          book.authors.map((author)=> (
+            <div className="book-authors" key={author}>{author}</div>
+          ))
+        )}
+      </div> 
+    </li>
+  )
 }
 
-export default Book
+Book.propTypes = {
+  book:  PropTypes.object.isRequired,
+  updateBook: PropTypes.func.isRequired
+}
+
+export default Book;
--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,39 +1,35 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Book from './Book'
 import PropTypes from 'prop-types'
 import sortBy from 'sort-by'
 
-class Shelf extends Component {
+const Shelf = (props) => {
 
-  static propTypes = {
-    books:  PropTypes.array.isRequired,
-    updateBooks: PropTypes.func.isRequired,
-    title: PropTypes.string.isRequired
-  } 
+  const { title, books, updateBooks } = props
+  books.sort(sortBy('title'))
 
-  render() {
-
-    const { title, books, updateBooks } = this.props
-
-    books.sort(sortBy('title'))
-    
-    return (
-      <div className="bookshelf" key={status}>
-        <h2 className="bookshelf-title">{title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {books.map((book) => (
-              <Book
-                key={book.id}
-                book={book}
-                updateBook={updateBooks}
-              />
-            ))}
-          </ol>
-        </div>
-      </div>    
-    )
-  }
+  return (
+    <div className="bookshelf" key={status}>
+      <h2 className="bookshelf-title">{title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.map((book) => (
+            <Book
+              key={book.id}
+              book={book}
+              updateBook={updateBooks}
+            />
+          ))}
+        </ol>
+      </div>
+    </div>    
+  )
 }
 
-export default Shelf
+Shelf.propTypes = {
+  books:  PropTypes.array.isRequired,
+  updateBooks: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired
+} 
+
+export default Shelf;
